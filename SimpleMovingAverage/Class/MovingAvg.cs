b5f01fa9540a209bfecff1433add0441b0fd9e5c
Daniil <zenitcomreal34@gmail.com>
2022-12-11 22:54:33 +0300

    add files

---
+++ SimpleMovingAverage/Class/MovingAvg.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SimpleMovingAverage.Class
+{
+    internal class MovingAvg
+    {
+        internal void Search(in string[] data, in int step,out List<double> avg)
+        {
+            avg = new List<double>();
+            double solution = 0;
+            for(int i = 0; i <= data.Length; i++)
+            {
+                if (i > (step - 1))
+                {
+                    for(int j = ((i-1)-(step-1)); j < i; j++)
+                    {
+                        solution+=Convert.ToDouble(data[j]);
+                    }
+                    avg.Add(Math.Round((solution/step),2));
+                    solution = 0;
+                }
+            }
+        }
+    }
+}
---
+++ SimpleMovingAverage/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using SimpleMovingAverage.Class;
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Input;
+
+namespace SimpleMovingAverage
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SearchAvg(object sender, RoutedEventArgs e)
+        {
+            lbAvg.ItemsSource = null;
+            string[] data;
+            if (txtArray.Text != string.Empty && txtStep.Text != string.Empty)
+            {
+                data = txtArray.Text.Split(',');
+                if (data.Length <= Convert.ToInt32(txtStep.Text))
+                    MessageBox.Show("Шаг больше, чем данных");
+                else
+                {
+                    MovingAvg moveAvg=new MovingAvg();
+                    moveAvg.Search(data, Convert.ToInt32(txtStep.Text), out List<double> avg);
+                    lbAvg.ItemsSource = avg;
+                    RenderGraph(avg);
+                }
+            }
+            else
+                MessageBox.Show("Заоплни данные");
+        }
+        private void RenderGraph(in List<double> avg)
+        {
+            graph.Visibility = Visibility.Visible;
+            double []dataX=new double[avg.Count];
+            double[] dataY = new double[avg.Count];
+            for(int i=0; i<avg.Count; i++)
+            {
+                dataX[i]=i;
+                dataY[i]=avg[i];
+            }
+            graph.Plot.AddScatter(dataX,dataY);
+            graph.Refresh();
+        }
+
+        private void txtArray_PreviewTextInput(object sender, TextCompositionEventArgs e)=>txtArray.TextWrapping = TextWrapping.Wrap;
+
+
+        private void txtStep_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (!char.IsDigit(e.Text, 0))
+                e.Handled = true;
+        }
+    }
+}
